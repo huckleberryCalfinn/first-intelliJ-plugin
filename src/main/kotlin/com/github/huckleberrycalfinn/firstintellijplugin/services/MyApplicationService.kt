3b8011db91f35cdff6bf4fed4bac30d5edb7296f
GitHub Action <action@github.com>
2022-07-22 16:04:54 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/huckleberrycalfinn/firstintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.huckleberrycalfinn.firstintellijplugin.services
+
+import com.github.huckleberrycalfinn.firstintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
